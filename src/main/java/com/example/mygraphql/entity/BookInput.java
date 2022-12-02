cd86e028273651e4a1cbfd2c0f645f0807d69aee
Chris <chrissantista@gmail.com>
2022-12-02 20:34:51 -0300

    removendo heranca do BookInput

--- src/main/java/com/example/mygraphql/entity/BookInput.java
+++ src/main/java/com/example/mygraphql/entity/BookInput.java
@@ -1,14 +1,9 @@
 package com.example.mygraphql.entity;
 
-import graphql.schema.GraphQLInputType;
 import lombok.Data;
 
 @Data
-public class BookInput implements GraphQLInputType {
-    @Override
-    public String getName() {
-        return "qwe";
-    }
+public class BookInput {
 
     private Long id;
     private String title;
